abcc6c91d53c57557ea71cb197a629b391759b9c
dci-student <oliver.sporl@dci-student.org>
2024-03-13 11:32:41 +0100

    proxy deleted

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(rateLimit({
 app.use(basicLogging(morgan));
 initMiddlewares(app);
 app.use(detailedLogging);
-app.set('trust proxy', 1); // Trust first proxy
+app.set('trust proxy', 1); 
 
 app.get('/', (req, res) => {
     res.send('Hallo Welt, dies ist ein Testserver mit erweiterten Logging-Funktionen!');
