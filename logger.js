3fe099fa2a917213be11d599f1704fe42736a8af
dci-student <oliver.sporl@dci-student.org>
2024-03-13 12:01:39 +0100

    geoip data updated, bug fixing

--- logger.js
+++ logger.js
@@ -1,9 +1,20 @@
-const LogEntry = require('./db'); 
+const LogEntry = require('./db');
 const geoip = require('geoip-lite');
+const morgan = require('morgan');
 
 function basicLogging(morgan) {
     return morgan('combined', async (tokens, req, res) => {
         try {
+            // Für Debugging-Zwecke: IP-Adresse loggen
+            console.log('IP Address:', req.ip);
+            
+            // Beispiel: Verwende eine feste öffentliche IP für das Testen (entfernen für Produktion)
+            const testIp = '8.8.8.8'; // Google Public DNS IP
+            const geoInfo = geoip.lookup(testIp);
+            
+            // Für Debugging-Zwecke: Geo-Informationen loggen
+            console.log('GeoInfo:', geoInfo);
+
             const logEntry = new LogEntry({
                 timestamp: new Date(),
                 ip: req.ip,
@@ -12,7 +23,7 @@ function basicLogging(morgan) {
                 userAgent: tokens['user-agent'](req, res),
                 referrer: tokens.referrer(req, res) || 'Keine Referenzseite',
                 deviceType: req.device.type,
-                geoInfo: geoip.lookup(req.ip),
+                geoInfo: geoInfo,
                 cookies: req.cookies
             });
 
@@ -26,7 +37,14 @@ function basicLogging(morgan) {
 
 async function detailedLogging(req, res, next) {
     try {
+        // Direktes Loggen der aktuellen Client-IP
+        console.log('IP Address:', req.ip);
+
         const geoInfo = geoip.lookup(req.ip);
+        
+        // Loggen der Geo-Informationen basierend auf der Client-IP
+        console.log('GeoInfo:', geoInfo);
+
         const deviceInfo = req.device.type;
         const referrer = req.get('Referrer') || 'Keine Referenzseite';
         const userAgent = req.get('User-Agent');
